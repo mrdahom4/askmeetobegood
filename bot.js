79310b78510f0baa088074452e58b1020d8fbca3
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-01-31 16:34:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1932,13 +1932,14 @@ client.on("message", message => {
 });
 
 
-
+const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
+ 
  
  
  
